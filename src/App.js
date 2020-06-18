e00ce30b3b0b5192ed845eba43de7b90015f318d
Musab Gulfam <musabgulfam0722@gmail.com>
2020-06-19 00:42:19 +0500

    Assignment: react app for Bootcamp 2020
    Made a react app with child element "Greeting""

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Greeting from './greeting'
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting name="Musab"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/greeting.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Greeting (props){
+   return <h1>Hello {props.name}<br/>Nice to have you here</h1>
+}
+
+export default Greeting
+
